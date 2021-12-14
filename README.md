8610fcbf78f19402beb37737841b988cfbf89951
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2021-12-13 16:27:53 -0800

    Initial commit: Version 0.0.1 of VS Code Plugin

---
+++ README.md
@@ -0,0 +1,32 @@
+# OpenAI Codex - VS Code Extension
+
+This is a VS Code Extension using OpenAI API. OpenAI’s API provides access to GPT-3, which performs a wide variety of natural language tasks, and Codex, which translates natural language to code.
+
+_Requires OpenAI API Key._ Create a free API Key at [OpenAI.com](https://openai.com/)
+
+Example Usage:
+
+- Highlight any section of text or code
+- Launch Command Pallete `CMD`+`SHFT`+`P`
+- Run command "OpenAI Autocomplete"
+
+## Local Installation
+
+To run locally, pull this repo and install all dependencies using yarn.
+
+```
+yarn install
+```
+
+Open the project in VS Code. Run the extension locally by pressing `F5`.
+
+## Developing
+
+This project is open source and open to contributions. Please create a pull request detailing any changes that are being made.
+
+Some TODO items:
+
+- Refactor using Typescript
+  - Having issues with imported modules (es5 vs es6)
+- Create a shortcut key for the autocomplete function
+- Allow user to select "davinci-codex" engine
---
+++ extension.js
@@ -0,0 +1,63 @@
+const vscode = require("vscode");
+const OpenAI = require("openai-api");
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  vscode.window.showInformationMessage(
+    "OpenAI Codex Autocomplete has been Activated."
+  );
+
+  let OPENAI_API_KEY = context.workspaceState.get("@codex.key", "");
+
+  async function setApiKey() {
+    let key = await vscode.window.showInputBox({
+      password: true,
+      title: "OpenAI API key",
+      prompt: "Enter API Key here...",
+    });
+    context.workspaceState.update("@codex.key", key);
+    OPENAI_API_KEY = context.workspaceState.get("@codex.key", "");
+  }
+
+  if (!OPENAI_API_KEY) {
+    vscode.window.showInformationMessage(
+      "API Key Required to use OpenAI Codex"
+    );
+    setApiKey();
+  }
+
+  // AutoComplete Function
+  context.subscriptions.push(
+    vscode.commands.registerCommand("openai-codex.autocomplete", async () => {
+      // Get API Key from Globalstate
+      if (!OPENAI_API_KEY) {
+        setApiKey();
+      }
+      const openai = new OpenAI(OPENAI_API_KEY);
+
+      let editor = vscode.window.activeTextEditor;
+      let selection = editor.selection;
+      let activeCursor = editor.selection.active;
+      let text = editor.document.getText(selection);
+
+      const completion = await openai.complete({
+        engine: "davinci",
+        prompt: text,
+        maxTokens: 100,
+      });
+
+      editor.edit((editBuilder) => {
+        editBuilder.insert(activeCursor, completion.data.choices[0].text);
+      });
+    })
+  );
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
